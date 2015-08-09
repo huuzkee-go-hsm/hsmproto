5b5c792aaa39ccfd2071f4f6a13e38f5a31d309f
Marcelle von Wendland <mvw@bancstreet.com>
2015-08-09 09:19:38 +0100

    init

---
+++ demo/demo.go
@@ -0,0 +1,32 @@
+package main
+
+import (		"fmt"   )
+
+//var rootDir string = "./testData"
+//type FileHash struct {
+//	fullpath       string
+//	hash           string
+//	lastModifyTime time.Time
+//}
+//var fileHashes []FileHash
+
+func main() {
+
+	//take CLI input
+	/*	dirPtr := flag.String("path", ".", " string")
+		svrPtr := flag.String("server", "www.softlayer.com", " string")
+		frqPtr := flag.Int("bkp-interval", 60, "backup interval in hours")
+		comprsPtr := flag.Bool("compression", false, "a bool")
+		encrptPtr := flag.Bool("encryption", false, "a bool")
+		flag.Parse()
+		dir_path := *dirPtr
+		server := *svrPtr
+		frequency := *frqPtr
+		compress := *comprsPtr
+		encrypt := *encrptPtr
+	*/ //I am not including bakcup run time since
+
+
+	fmt.Printf("Hello World !  Call:  %v of %v \n", 1, 3 )
+}
+
