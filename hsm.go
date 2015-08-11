1e9fff83d1b1ac6895b5fdf90ad6b0d92c3c17aa
Marcelle von Wendland <mvw@hivves.com>
2015-08-11 17:49:32 +0100

    init

--- hsm.go
+++ hsm.go
@@ -1,38 +1,50 @@
 package hsmproto
 
+// SYSTEM STATE TOKENS ///////////////////////////////////////////////////////////
 
-type State struct {
-	StateType     int
-	StateObject   HsmState
-}
+const ( 
+ 	HSM_SYSSTAT_NULL     = (0 + iota)		// NULL PSEUDO STATE
+ 	HSM_SYSSTAT_ACTIVATE				// NULL PSEUDO STATE
+ 	HSM_SYSSTAT_LIVE				// NULL PSEUDO STATE
+ 	HSM_SYSSTAT_DEACTIVATE 			      	// NULL PSEUDO STATE
+ 	HSM_SYSSTAT_DEAD 			      	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+ 	HSM_SYSSTAT_INITIALISING	        	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+  	HSM_SYSSTAT_STARTTRANSITION		        // NULL PSEUDO STATE
+ 	HSM_SYSSTAT_EXITCHAIN		         	// NULL PSEUDO STATE
+  	HSM_SYSSTAT_ENTRYCHAIN			      	// NULL PSEUDO STATE
+   	HSM_SYSSTAT_COMPLETETRANSITION		        // NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+  	HSM_SYSSTAT_REQUESTNEWERA		      	// NULL PSEUDO STATE
+  	HSM_SYSSTAT_PREPARENEWERA		        // NULL PSEUDO STATE
+  	HSM_SYSSTAT_NEWERA		     		// NULL PSEUDO STATE
+  	HSM_SYSSTAT_COMMITNEWERA		     	// NULL PSEUDO STATE  
+ 	//------------------------------------------------------------------
+  	HSM_SYSSTAT_RESTORING			      	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+  	HSM_SYSSTAT_REQUESTSYNCPOINT		      	// NULL PSEUDO STATE
+  	HSM_SYSSTAT_PREPARESYNCPOINT		      	// NULL PSEUDO STATE
+  	HSM_SYSSTAT_SYNCPOINT		      		// NULL PSEUDO STATE
+  	HSM_SYSSTAT_COMMITSYNCPOINT		      	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+   	HSM_SYSSTAT_REQUESTCONFIG		      	// NULL PSEUDO STATE
+   	HSM_SYSSTAT_PREPARECONFIGT		      	// NULL PSEUDO STATE
+   	HSM_SYSSTAT_CONFIG		      		// NULL PSEUDO STATE
+  	HSM_SYSSTAT_COMMITCONFIG		      	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+ 	HSM_SYSSTAT_TOP		      			// NULL PSEUDO STATE
+ 	HSM_SYSSTAT_USER		      		// NULL PSEUDO STATE
+)
+
+
+/////
 
 type HsmState interface {
 
 	AcceptState () int
 
 }
-
-
-const ( 
- 	HSM_SYSSTAT_NULL     = (0 + iota)		// NULL PSEUDO STATE
- 	HSM_SYSSTAT_ACTIVATE				// NULL PSEUDO STATE
- 	HSM_SYSSTAT_DEACTIVATE 			      	// NULL PSEUDO STATE
- 	HSM_SYSSTAT_INITIALISE		        	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_TRANSITION		        	// NULL PSEUDO STATE
- 	HSM_SYSSTAT_EXITCHAIN		         	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_ENTRYCHAIN			      	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_REQUESTNEWERA		      	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_STARTNEWERA		        	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_AWAITNEWERA			      	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_RESTOREERA			      	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_REQUESTSYNCPOINT		      	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_SYNCPOINT		      		// NULL PSEUDO STATE
-  	HSM_SYSSTAT_COMPLETESYNCPOINT		      	// NULL PSEUDO STATE
- 	HSM_SYSSTAT_TOP		      			// NULL PSEUDO STATE
- 	HSM_SYSSTAT_USER		      		// NULL PSEUDO STATE
-)
-
-
 type HsmEngine struct {
 
 	LastState 	HsmState 
@@ -41,6 +53,7 @@ type HsmEngine struct {
 }
 
 
+//////
 
 
 type HSM interface {
@@ -60,7 +73,22 @@ type MyHSM struct {
 	
 }
 
+////////////////////////////////////////////////////////////////////////////
+// USER SPECIFIC IMPLEMENTATION ////////////////////////////////////////////
 
 func ( myhsm MyHSM ) Activate()  {
     
 } 
+
+
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
+type State struct {
+	StateType     int
+	StateObject   HsmState
+}
