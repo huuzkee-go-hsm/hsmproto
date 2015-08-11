36fedf7f869d70cc17fedd77d3187fe36be2fbeb
Marcelle von Wendland <mvw@bancstreet.com>
2015-08-11 14:07:10 +0100

    init

--- hsm.go
+++ hsm.go
@@ -1,8 +1,47 @@
 package hsmproto
 
 
-type Listener interface {
+type State struct {
+	StateType     int
+	StateObject   HsmState
+}
 
-	Listen () int
+type HsmState interface {
+
+	AcceptState () int
 
 }
+
+
+
+type HsmEngine struct {
+
+	LastState 	HsmState 
+	CurrentState 	HsmState 
+	
+}
+
+
+
+
+type HSM interface {
+
+	Activate()  
+
+}
+
+
+type MyHSM struct {
+
+	HsmEngine
+	
+	STATE_01 	State_01 
+	STATE_01_01 	State_01_01 
+	STATE_02 	State_02 
+	
+}
+
+
+func ( myhsm MyHSM ) Activate()  {
+    
+} 
--- state-01-01.go
+++ state-01-01.go
@@ -6,5 +6,13 @@ type State_01_01 struct {
 
 
 func ( s11 State_01_01 ) Listen () int {
+
+	for {
+
+
+
+	}
+
+
     return 88
 }
