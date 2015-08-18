9fc5934a37332df193b765bf360187190f6431ca
Marcelle von Wendland <mvw@bancstreet.com>
2015-08-18 09:45:47 +0100

    init

--- hsm.go
+++ hsm.go
@@ -6,38 +6,6 @@ const (
  	HSM_SYSSTAT_NULL     = (0 + iota)		// NULL PSEUDO STATE
  	HSM_SYSSTAT_LIVE				// NULL PSEUDO STATE
  	HSM_SYSSTAT_DEAD 			      	// NULL PSEUDO STATE
- 	HSM_SYSSTAT_ACTIVATE				// NULL PSEUDO STATE
-
- 	HSM_SYSSTAT_DEACTIVATE 			      	// NULL PSEUDO STATE
-
- 	//------------------------------------------------------------------
- 	HSM_SYSSTAT_INITIALISING	        	// NULL PSEUDO STATE
- 	//------------------------------------------------------------------
-  	HSM_SYSSTAT_STARTTRANSITION		        // NULL PSEUDO STATE
- 	HSM_SYSSTAT_EXITCHAIN		         	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_ENTRYCHAIN			      	// NULL PSEUDO STATE
-   	HSM_SYSSTAT_COMPLETETRANSITION		        // NULL PSEUDO STATE
- 	//------------------------------------------------------------------
-  	HSM_SYSSTAT_REQUESTNEWERA		      	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_PREPARENEWERA		        // NULL PSEUDO STATE
-  	HSM_SYSSTAT_NEWERA		     		// NULL PSEUDO STATE
-  	HSM_SYSSTAT_COMMITNEWERA		     	// NULL PSEUDO STATE  
- 	//------------------------------------------------------------------
-  	HSM_SYSSTAT_RESTORING			      	// NULL PSEUDO STATE
- 	//------------------------------------------------------------------
-  	HSM_SYSSTAT_REQUESTSYNCPOINT		      	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_PREPARESYNCPOINT		      	// NULL PSEUDO STATE
-  	HSM_SYSSTAT_SYNCPOINT		      		// NULL PSEUDO STATE
-  	HSM_SYSSTAT_COMMITSYNCPOINT		      	// NULL PSEUDO STATE
- 	//------------------------------------------------------------------
-   	HSM_SYSSTAT_REQUESTCONFIG		      	// NULL PSEUDO STATE
-   	HSM_SYSSTAT_PREPARECONFIGT		      	// NULL PSEUDO STATE
-   	HSM_SYSSTAT_CONFIG		      		// NULL PSEUDO STATE
-  	HSM_SYSSTAT_COMMITCONFIG		      	// NULL PSEUDO STATE
- 	//------------------------------------------------------------------
- 	HSM_SYSSTAT_CURRENTSTATE      			// NULL PSEUDO STATE
- 	//------------------------------------------------------------------
- 	HSM_SYSSTAT_TOP		      			// NULL PSEUDO STATE
  	HSM_SYSSTAT_USER		      		// NULL PSEUDO STATE
 )
 
@@ -49,19 +17,23 @@ type HsmState interface {
 	AcceptState () int
 
 }
+
 type HsmActor struct {
 
 	LastState 	HsmState 
 	CurrentState 	HsmState 
-
 	
 }
 
 func ( hsmengine HsmActor) Live()  {
 
-	state := HSM_SYSSTAT_NULL
-	for state != HSM_SYSSTAT_DEAD {
-		state += 1
+	LastState 	:= HSM_SYSSTAT_NULL
+	CurrentState	:= HSM_SYSSTAT_NULL
+	
+	for CurrentState != HSM_SYSSTAT_DEAD {
+	
+		CurrentState += 1
+		
 	}
 
     
@@ -107,3 +79,41 @@ type State struct {
 	StateType     int
 	StateObject   HsmState
 }
+
+
+//////////////////////////////////////////////////////////////////////////////
+
+/*
+ 	HSM_SYSSTAT_ACTIVATE				// NULL PSEUDO STATE
+ 	HSM_SYSSTAT_DEACTIVATE 			      	// NULL PSEUDO STATE
+
+ 	//------------------------------------------------------------------
+ 	HSM_SYSSTAT_INITIALISING	        	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+  	HSM_SYSSTAT_STARTTRANSITION		        // NULL PSEUDO STATE
+ 	HSM_SYSSTAT_EXITCHAIN		         	// NULL PSEUDO STATE
+  	HSM_SYSSTAT_ENTRYCHAIN			      	// NULL PSEUDO STATE
+   	HSM_SYSSTAT_COMPLETETRANSITION		        // NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+  	HSM_SYSSTAT_REQUESTNEWERA		      	// NULL PSEUDO STATE
+  	HSM_SYSSTAT_PREPARENEWERA		        // NULL PSEUDO STATE
+  	HSM_SYSSTAT_NEWERA		     		// NULL PSEUDO STATE
+  	HSM_SYSSTAT_COMMITNEWERA		     	// NULL PSEUDO STATE  
+ 	//------------------------------------------------------------------
+  	HSM_SYSSTAT_RESTORING			      	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+  	HSM_SYSSTAT_REQUESTSYNCPOINT		      	// NULL PSEUDO STATE
+  	HSM_SYSSTAT_PREPARESYNCPOINT		      	// NULL PSEUDO STATE
+  	HSM_SYSSTAT_SYNCPOINT		      		// NULL PSEUDO STATE
+  	HSM_SYSSTAT_COMMITSYNCPOINT		      	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+   	HSM_SYSSTAT_REQUESTCONFIG		      	// NULL PSEUDO STATE
+   	HSM_SYSSTAT_PREPARECONFIGT		      	// NULL PSEUDO STATE
+   	HSM_SYSSTAT_CONFIG		      		// NULL PSEUDO STATE
+  	HSM_SYSSTAT_COMMITCONFIG		      	// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+ 	HSM_SYSSTAT_CURRENTSTATE      			// NULL PSEUDO STATE
+ 	//------------------------------------------------------------------
+ 	HSM_SYSSTAT_TOP		      			// NULL PSEUDO STATE
+ 	
+*/
